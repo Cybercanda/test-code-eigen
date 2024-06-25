f6719abfff1347d7ee9f985ec8746a2e602934e3
Cybercanda <bilyworkspaces@gmail.com>
2024-06-26 06:42:35 +0700

    feat: adding swagger documentation & readme

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
 
 app.use(express.json());
+
+const swaggerOptions = {
+    swaggerDefinition: swaggerDocument,
+    apis: ['app.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/api', routes)
 
